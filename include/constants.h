b876cd2473a020c5a365d2543b9341acb2790af2
Ariel Roque <43178469+pvtoari@users.noreply.github.com>
2025-08-24 12:50:41 +0200

    Implement pywal argument and routine

--- include/constants.h
+++ include/constants.h
@@ -4,9 +4,14 @@
 #define DEFAULT_PERIOD 1000
 #define DEFAULT_MPVPAPER_SOCKET_PATH "/tmp/mpvsocket"
 #define DEFAULT_MPVPAPER_SOCKET_WAIT_TIME 5000
+#define TEMP_DIR "/tmp/mpvpaper-stop"
+
 #define QUERY_HYPRLAND_SOCKET_ACTIVE_WORKSPACE "j/activeworkspace"
+
 #define QUERY_MPVPAPER_SOCKET_PAUSE_PROPERTY "{ \"command\": [\"get_property\", \"pause\"]}\n"
+#define QUERY_MPVPAPER_SOCKET_DO_SCREENSHOT "{ \"command\": [\"screenshot\"] }\n"
 #define SET_MPVPAPER_SOCKET_PAUSE "{ \"command\": [\"set_property\", \"pause\", true] }\n"
 #define SET_MPVPAPER_SOCKET_RESUME "{ \"command\": [\"set_property\", \"pause\", false] }\n"
+#define SET_MPVPAPER_SCREENSHOT_DIR "{ \"command\": [\"set_property\", \"screenshot-dir\", \""TEMP_DIR"\"] }\n"
 
 #endif
